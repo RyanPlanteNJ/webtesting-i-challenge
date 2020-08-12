310d172f1335431bd1216f2dbf41d6114034450c
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-08-11 22:18:54 -0400

    MVP met. possible stretch

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,15 +5,29 @@ module.exports = {
   get,
 };
 
-function success(item) {
-  return { ...item };
+function success( item) {
+if(item.enhancement < 20) {
+  item.enhancement += 1
+}
+    return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15){
+    item.durability -= 5
+  } else if(item.enhancement >= 15) {
+    if(item.enhancement > 16) {
+      item.enhancement -= 1
+    }
+    item.durability -= 10
+  }
   return { ...item };
 }
 
 function repair(item) {
+  if(item.durability < 100) {
+    item.durability = 100
+  }
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,28 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('item enhnacement', () => {
+const item = {name: "Demon Monarch's Ring", durability: 50, enhancement: 10};
+  describe('repaired item', () => {
+
+    it('restores weapon to 100 durability', () => {
+      const repaired = { ...item, durability: 100 }
+      expect(enhancer.repair(item)).toEqual(repaired);
+    })
+  })
+
+  describe('increased enhancement', () => {
+    it('should increase enhancement by 1, if not 20', () => {
+      const enhanced = { ...item, enhancement: 11 }
+      expect(enhancer.success(item)).toEqual(enhanced)
+    })
+  })
+
+  describe('item failed to enhanced, loses durability and enhancement', () => {
+    const item = {name: "Demon Monarch's Ring", durability: 50, enhancement: 20};
+    it('reduces enhancement and durabiilty', () => {
+      const lost = {...item, durability: 40, enhancement: 19};
+      expect(enhancer.fail(item)).toEqual(lost)
+    })
+  })
+})
